ddf93ca06a19e25adb8dde00ef38dfa84b0dc45d
Denis Sotnikov <sport_piter@mail.ru>
2021-02-19 19:02:02 +0300

    1. PsqlStore. [#469052]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,67 +1,16 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import ru.job4j.dream.model.Post;
-import ru.job4j.dream.model.Candidate;
+public interface Store {
+    Collection<Post> findAllPosts();
 
-public class Store {
+    Collection<Candidate> findAllCandidates();
 
-    private static AtomicInteger postid = new AtomicInteger(3);
-    private static AtomicInteger canid = new AtomicInteger(3);
+    void save(Post post);
 
-    private static final Store INST = new Store();
-
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-
-    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-
-    private Store() {
-        posts.put(1, new Post(1, "Junior Java Job", "Стартовая позиция", new Date()));
-        posts.put(2, new Post(2, "Middle Java Job", "Средняя позиция", new Date()));
-        posts.put(3, new Post(3, "Senior Java Job", "Высокая позиция", new Date()));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
-    }
-
-    public void save(Post post) {
-        if (post.getId() == 0) {
-            post.setId(postid.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
-
-    public Post findById(int id) {
-        return posts.get(id);
-    }
-
-    public Candidate findCandidateById(int id) {
-        System.out.println(candidates.get(id));
-        return candidates.get(id);
-    }
-
-    public void save(Candidate candidate) {
-        if (candidate.getId() == 0) {
-            candidate.setId(canid.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
-
-    public static Store instOf() {
-        return INST;
-    }
-
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
+    Post findById(int id);
 }
