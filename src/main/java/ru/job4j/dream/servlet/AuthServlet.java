1b4762d1906ece4b6490bef938c03cb6e06e071b
Denis Sotnikov <sport_piter@mail.ru>
2021-02-21 13:36:20 +0300

    4. Регистрация пользователя. [#469088]

--- src/main/java/ru/job4j/dream/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/dream/servlet/AuthServlet.java
@@ -18,13 +18,13 @@ public class AuthServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         User user = store.findUserByEmail(email);
-        if (user == null) {
-            req.setAttribute("error", "Не верный email или пароль");
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
-        } else {
+        if (user != null && user.getPassword().equals(password)) {
             HttpSession sc = req.getSession();
             sc.setAttribute("user", user);
             resp.sendRedirect(req.getContextPath() + "/index.do");
+        } else {
+            req.setAttribute("error", "Не верный email или пароль");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
         }
     }
 }
