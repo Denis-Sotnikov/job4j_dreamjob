b6da81de847e4534e7593bb95f751ce0e00376a6
Denis Sotnikov <sport_piter@mail.ru>
2021-02-25 04:17:30 +0300

    4.1. JSON формат. [#469058]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,6 +1,8 @@
 package ru.job4j.dream.servlet;
 
 
+import org.json.simple.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +14,12 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        System.out.println(name);
-        writer.println("Nice to meet you, " + name);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        JSONObject json = new JSONObject();
+        json.put("message", "Nice to meet you, " + req.getParameter("name"));
+        writer.println(json);
         writer.flush();
     }
 }
